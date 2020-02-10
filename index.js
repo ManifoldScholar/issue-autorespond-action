57ec06a8efa9dee5772cc410435fdef140dd40b9
Zach Davis <zach@castironcoding.com>
2020-02-10 13:57:20 -0800

    [WIP]

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 const core = require('@actions/core');
+const github = require('@actions/github');
 const wait = require('./wait');
 
 // most @actions toolkit packages have async methods
 async function run() {
   try {
     const config = JSON.parse(core.getInput("config"));
-    console.log(config);
+
+    if (!Array.isArray(config)) throw "Config must be an array"
+
+    const issue = github.context.payload.issue;
+    if (!issue) return console.log("No issue. Exiting gracefully");
+
+    const issueLabels = issue.labels.map((label) => label.name);
+
+    config.forEach((entry) => {
+
+      const match = entry.require.every((label) => issueLabels.includes(label));
+      console.log(match, entry.require);
+
+    })
+
+
 
   }
   catch (error) {
